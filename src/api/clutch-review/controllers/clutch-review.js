b7ff7e615b7f2d2541db4537a58ee812cf1c517c
Shoaib Ali <shoaib.ali@agencypartner.com>
2026-05-22 22:34:00 +0500

    clutch review added in api section

---
+++ src/api/clutch-review/controllers/clutch-review.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * clutch-review controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::clutch-review.clutch-review');
---
+++ src/api/clutch-review/routes/clutch-review.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * clutch-review router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::clutch-review.clutch-review');
