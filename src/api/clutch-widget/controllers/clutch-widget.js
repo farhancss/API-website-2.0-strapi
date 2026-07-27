c02118c4ff62a447f5caa79b36324c40d5158167
Shoaib Ali <shoaib.ali@agencypartner.com>
2026-07-27 19:00:18 +0500

    Clutch widget field added in hero page

---
+++ src/api/clutch-widget/controllers/clutch-widget.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * clutch-widget controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::clutch-widget.clutch-widget');
---
+++ src/api/clutch-widget/routes/clutch-widget.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * clutch-widget router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::clutch-widget.clutch-widget');
