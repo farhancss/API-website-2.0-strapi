459bc7c6903e900eeacc5d6a199090b3aab43e60
Shoaib Ali <shoaib.ali@agencypartner.com>
2026-08-20 22:01:05 +0500

    Add hoverIcon field to technology schema for media support

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1117,6 +1117,7 @@ export interface ApiTechnologyTechnology extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    hoverIcon: Schema.Attribute.Media<'images'>;
     icon: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
